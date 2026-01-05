0865ac81d1b080f466f9e23d79f0ceb972ddbc36
Ziqiao Lin <lin978067802@gmail.com>
2026-01-05 19:14:47 +1100

    function infix to postfix

---
+++ algorithm/postfix expression.ts
@@ -0,0 +1,109 @@
+import { LinkedListStack } from "../linked-list-stack.js";
+
+function tokenize(exp: string): string[] {
+  // \d+(\.\d+)? match number include float
+  // \*\*|[+\-*/%()] match operator and parentheses
+  const tokens: string[] = [];
+  const regex = /(\d+(\.\d+)?|\*\*|[+\-*/%()])/g;
+  // remove all space
+  exp = exp.replace(/\s+/g, '');
+  const rawTokens = exp.match(regex) || [];
+  for(let i = 0; i < rawTokens.length; i++) {
+    const curr = rawTokens[i]!;
+    if(curr === '-') {
+      const prev = tokens[tokens.length - 1]
+      // check nagetive sign or minus sign
+      const isNegativeSign = tokens.length === 0 || 
+      prev !== undefined && ['(', '+', '-', '/', '%', '*', '**'].includes(prev);
+      if(isNegativeSign && i + 1 < rawTokens.length) {
+        tokens.push('-' + rawTokens[++i]);
+        continue
+      } 
+    }
+    tokens.push(curr);
+  }
+  return tokens;
+}
+
+function isOperand(s: string): boolean { 
+  const operators = ['+', '-', '/', '%', '*', '**', ')', '('];
+  return !operators.includes(s);
+}
+
+function isOperator(s: string): boolean {
+  const operators = ['+', '-', '/', '%', '*', '**'];
+  return operators.includes(s);
+}
+
+function isOpeningParentheses(s: string): boolean {
+  return s === '(';
+}
+
+const ops: Record<string, {prec: number, assoc: 'L' | 'R'}> = {
+  '+': { prec: 2, assoc: 'L' },
+  '-': { prec: 2, assoc: 'L' },
+  '*': { prec: 3, assoc: 'L' },
+  '/': { prec: 3, assoc: 'L' },
+  '%': { prec: 3, assoc: 'L' },
+  '**': { prec: 4, assoc: 'R' }
+};
+
+function shouldPop(s1: string, s2: string): boolean {
+  // s2: stack top
+  if(isOpeningParentheses(s2)) return false;
+
+  const p1 = ops[s1]!.prec; //curr
+  const p2 = ops[s2]!.prec; //top
+  const a1 = ops[s1]!.assoc;
+
+  return a1 === 'L' ? p1 <= p2 : p1 < p2;
+}
+
+function infixToPostfix(exp: string): string {
+  const expression = tokenize(exp);
+  if(expression.length === 0) return '';
+  const stack = new LinkedListStack<string>();
+  let res: string = '';
+  for(let i = 0; i < expression.length; i++) {
+    if(isOperand(expression[i]!)) {
+      // if it's operand, put it in the postfix expression
+      res += expression[i] + ' ';
+    } else if(isOperator(expression[i]!)) {
+      // if it's operator, check the top of stack, if it's [+ - * / %], higher or equal precedance operator in the stack will pop to postfix exp;
+      //                                           if it's [**] only higher precedance will pop.
+      // stop when top stack is '('.
+      while(!stack.isEmpty() && shouldPop(expression[i]!, stack.top()!)) {
+        res += stack.pop() + ' ';
+      }
+      stack.push(expression[i]!);
+    } else if(isOpeningParentheses(expression[i]!)) {
+      // if it's a opening parentheses, push it to stack
+      stack.push(expression[i]!);
+    } else if(expression[i] === ')') {
+      // if it's a closing parentheses, pop all operator until a opening parentheses.
+      while(!stack.isEmpty() && !isOpeningParentheses(stack.top()!)) {
+        res += stack.pop() + ' ';
+      }
+      stack.pop(); // pop '('
+    }
+  }
+  while(!stack.isEmpty()) {
+  // pop all operator until stack empty.
+    res += stack.pop() + ' ';
+  }
+  return res;
+}
+
+const exp1 = '3 + 4 * 2 / 1'; //3 4 2 * 1 / +
+const exp2 = '((1 + 2) * 3) / (4 + 5)'; //1 2 + 3 * 4 5 + /
+const exp3 = '3 + 2 ** 2 ** 3'; //3 2 2 3 ** ** +
+const exp4 = '5 + (-3 + 2) * 4'; //5 -3 2 + 4 * +
+const exp5 = '100 / (2 + 3) ** 2 * 3'; //100 2 3 + 2 ** / 3 *
+const exp6 = '32 ** 1 ** 5 + 3 ** 2'; //32 1 5 ** ** 3 2 ** +
+
+console.log(infixToPostfix(exp1));
+console.log(infixToPostfix(exp2));
+console.log(infixToPostfix(exp3));
+console.log(infixToPostfix(exp4));
+console.log(infixToPostfix(exp5));
+console.log(infixToPostfix(exp6));
